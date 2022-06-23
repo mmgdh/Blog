9080a9777a4422c3d365a53c2962e9883ca70e6b
林庆汉 <mmgdh@sina.com>
2022-06-23 17:25:29 +0800

    1.尝试添加公用配置进行迁移（失败） 2.config加上数据库连接字符串

--- DotNetCore-blog/BlogWebAPI/BlogWebAPI/Program.cs
+++ DotNetCore-blog/BlogWebAPI/BlogWebAPI/Program.cs
@@ -1,7 +1,10 @@
+using CommonInitializer;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.ConfigureDbConfiguration();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ DotNetCore-blog/BlogWebAPI/CommonInitializer/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CommonInfrastructure;
+
+namespace CommonInitializer
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static void ConfigureDbConfiguration(this WebApplicationBuilder builder)
+        {
+            string connStr = builder.Configuration.GetValue<string>("ConnectionStrings:SqlServer");
+            builder.Services.AddDbContext<BaseDbContext>(option =>
+            {
+                option.UseSqlServer(connStr);
+            });
+            //builder.Host.ConfigureAppConfiguration((hostCtx, configBuilder) =>
+            //{
+            //    //不能使用ConfigureAppConfiguration中的configBuilder去读取配置，否则就循环调用了，因此这里直接自己去读取配置文件
+            //    var configRoot = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            //    string connStr = configRoot.GetValue<string>("DefaultDB:ConnStr");
+            //    string connStr = builder.Configuration.GetValue<string>("DefaultDB:ConnStr");
+            //    builder.Services.AddDbContext<BaseDbContext>(option =>
+            //    {
+            //        option.UseSqlServer(connStr)
+            //    });
+            //});
+        }
+    }
+
+}
