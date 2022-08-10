32a1ee18271f26c4138887cc11da35c3084ee4dc
mmgdh <976983151@qq.com>
2022-08-10 08:14:05 +0800

    博客主页修饰

--- vue-blog/src/components/Blog/BlogMenu.vue
+++ vue-blog/src/components/Blog/BlogMenu.vue
@@ -1,37 +1,35 @@
 <template>
-  <a-menu v-model:selectedKeys="current" mode="horizontal">
-    <a-menu-item key="mail">
-      <template #icon>
-        <mail-outlined />
-      </template>
-      Navigation One
-    </a-menu-item>
-    <a-menu-item key="app" disabled>
-      <template #icon>
-        <appstore-outlined />
-      </template>
-      Navigation Two
-    </a-menu-item>
-    <a-sub-menu key="sub1">
-      <template #icon>
-        <setting-outlined />
-      </template>
-      <template #title>Navigation Three - Submenu</template>
-      <a-menu-item-group title="Item 1">
-        <a-menu-item key="setting:1">Option 1</a-menu-item>
-        <a-menu-item key="setting:2">Option 2</a-menu-item>
-      </a-menu-item-group>
-      <a-menu-item-group title="Item 2">
-        <a-menu-item key="setting:3">Option 3</a-menu-item>
-        <a-menu-item key="setting:4">Option 4</a-menu-item>
-      </a-menu-item-group>
-    </a-sub-menu>
-    <a-menu-item key="alipay">
-      <a href="https://antdv.com" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    </a-menu-item>
-  </a-menu>
+  <div class="navBody">
+    <nav class="nav">
+      <div class="container">
+        <h1 class="logo"><a href="#">主页</a></h1>
+        <ul>
+          <li><a href="#" class="current">Home</a> </li>
+          <li><a href="#">T1</a> </li>
+          <li><a href="#">T2</a> </li>
+          <li><a href="#">T3</a> </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="hero">
+      <div class="container">
+        <h1>Welcome To My Website</h1>
+        <p>Picture,Techonolege...</p>
+      </div>
+    </div>
+
+    <section class="container content">
+      <h2>Content Title One</h2>
+      <p>Picture,Techonolege...</p>
+
+      <h3>Content Two</h3>
+      <p>Lorem</p>
+    </section>
+  </div>
+
+
+
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
@@ -50,3 +48,120 @@ export default defineComponent({
   },
 });
 </script>
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.navBody {
+  font-family: 'Open Sans', sans-serif;
+  color: #FFF;
+  padding-bottom: 50px;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.nav {
+  position: fixed;
+  background-color: #222;
+  top: 0;
+  left: 0;
+  right: 0;
+  transition: all 0.3s ease-in-out;
+}
+
+.nav .container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0;
+  transition: all 0.3s ease-in-out;
+}
+
+.nav ul {
+  display: flex;
+  list-style-type: none;
+  align-items: center;
+  justify-content: center;
+}
+
+.nav a {
+  color: #fff;
+  text-decoration: none;
+  padding: 7px 15px;
+  transition: all 0.3s ease-in-out;
+}
+
+.nav.active {
+  background-color: #fff;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+}
+
+.nav.active a {
+  color: #000
+}
+
+.nav.active .container {
+  padding: 10px 0;
+}
+
+.nav a.current,
+.nav a:hover {
+  color: #c0392b;
+  font-weight: bold;
+}
+
+.hero {
+  background-image: url('../../Img/Background.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: bottom center;
+  height: 100vh;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  position: relative;
+  margin-bottom: 20px;
+  z-index: -2;
+}
+
+.hero::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: -1;
+}
+
+.hero h1 {
+  font-size: 46px;
+  margin: -20px 0 20px;
+}
+
+.hero p {
+  font-size: 20px;
+  letter-spacing: 1px;
+}
+
+.content h2,
+.content h3 {
+  font-size: 150%;
+  margin: 20px 0;
+}
+
+.content p {
+  /* color: #555; */
+  line-height: 30px;
+  letter-spacing: 1.2px;
+}
+</style>
---
+++ vue-blog/src/components/User/Login.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="login-container">
+    <a-form :model="form" class="login-form" @finish="onSubmit">
+      <h1 class="title">LOGIN</h1>
+      <a-form-item name="userName">
+        <a-input v-model:value="form.userName">
+          <a-icon slot="prefix" type="user" />
+        </a-input>
+      </a-form-item>
+      <a-form-item name="passWord">
+        <a-input-password v-model:value="form.passWord">
+          <a-icon slot="prefix" type="unlock" />
+        </a-input-password>
+      </a-form-item>
+      <a-form-item>
+        <a-button shape="round" class="submit">保存</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { UserLogin } from '../../Entities/E_Users.js';
+import UserLoginService from '../../Services/UserLoginService.js';
+import { useRouter } from 'vue-router';
+
+let router = useRouter()
+var _form: UserLogin = {
+  userName: "",
+  passWord: "",
+}
+var form = ref(_form);
+
+const onSubmit = (value: UserLogin) => {
+  UserLoginService.prototype.Login(value).then(token => {
+    if (token != "") {
+      localStorage.setItem('JWT', token);
+      console.log("登陆成功！");
+      router.push('/ArticleTable')
+    }
+
+  });
+
+}
+
+</script>
+<style scoped>
+.login-form {
+  width: 560px;
+  height: 30px;
+  margin: 0 auto;
+  /* background: url("../../assets/houTaiKuang.png"); */
+  padding: 40px 110px;
+}
+
+/* 背景 */
+.login-container {
+  /* background: url("../../assets/BackGroud.jpg") no-repeat; */
+  background-size: 100% 100%;
+  background-color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.login-form {
+
+  /* 超出部分隐藏 */
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 500px;
+  height: 450px;
+  background-color: #34495e;
+  border-radius: 10px;
+  box-shadow: 10px 10px 20px rgba(33, 44, 55, .3);
+
+}
+
+/* 登陆按钮 */
+.submit {
+  width: 100%;
+  height: 45px;
+  font-size: 16px;
+}
+
+/* 用户登陆标题 */
+.title {
+  margin-bottom: 50px;
+  color: #fff;
+  font-weight: 700;
+  font-size: 24px;
+  font-family: Microsoft Yahei;
+  -ms-flex-align: center;
+}
+</style>
